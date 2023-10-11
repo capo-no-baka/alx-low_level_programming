f9e8bc247d84d694ee1b81a8af1a63e13c8392fa
capo-no-baka <capo.king1999@gmail.com>
2023-10-10 17:15:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	int batata;
+
+	for (batata = 97; batata < 123; batata++)
+		_putchar(batata);
+
+	_putchar('\n');
+}
+
