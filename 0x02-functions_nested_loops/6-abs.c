89cfaac66c7cb1aadd3030884f676f3811a67a1d
capo-no-baka <capo.king1999@gmail.com>
2023-10-10 18:47:13 -0700

    task 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @v: the integer to find the absolute value of
+ * Return: the absolute value of n
+ */
+int _abs(int v)
+{
+	if (v < 0)
+		return (-v);
+	return (v);
+}
+
